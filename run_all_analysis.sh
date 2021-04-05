ac420e95e878a7f8829cf4b91fe60c638c25c11e
ioannisa92 <ianastop@ucsc.edu>
2021-04-05 22:18:19 +0000

    submission

--- run_all_analysis.sh
+++ run_all_analysis.sh
@@ -13,13 +13,15 @@ python exclude_bases.py -i ./ont_models/r9.4_180mv_70bps_5mer_5to3_RNA.model -o 
 python exclude_bases.py -i ./ont_models/r9.4_180mv_450bps_6mer_DNA.model -o dna_exclude_basepairs_results.npy -n_type $3 'DNA' -base_pair_exclude
 python exclude_bases.py -i ./ont_models/r9.4_180mv_70bps_5mer_5to3_RNA.model -o rna_exclude_basepairs_results.npy -n_type $3 'RNA' -base_pair_exclude
 
-python dna_mod_trainpred.py -i ./ont_models/r9.4_180mv_450bps_6mer_DNA.model  -o dna_mod_trainpred_results_50fold_run2.npy
+# de novo modification prediction
+python dna_mod_pred.py -i  ./ont_models/r9.4_180mv_450bps_6mer_DNA.model -model_fn dna_model -o dna_mod_pred_50repeat_results.npy
+
+# de novo and fractions of modified kmers included in training
+python dna_mod_trainpred.py -i ./ont_models/r9.4_180mv_450bps_6mer_DNA.model  -o dna_mod_trainpred_results_50fold.npy
 
 
 
 
-#command: ["bash", "combined_cv_wrap.sh", "0", "combined_cv_results.npy",  "0.9 0.75 0.5 0.25 0.1"]
-'bash','dna_mod_pred.sh', './ont_models/r9.4_180mv_450bps_6mer_DNA.model','dna_model', 'dna_mod_pred_results_50repeat_run1.npy'
-'bash','rna_mod_pred.sh', './ont_models/r9.4_GSE124309_atgc_xtgc.model', 'rna_model','rna_mod_pred_results_repeat50times'
+
 
 
